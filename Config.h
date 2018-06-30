3716860ec6adc6e2bead27c3e48accd632c97a5c
vjdagi <3862730+vjdagi@users.noreply.github.com>
2018-06-30 16:55:43 +0200

    safety first

---
+++ Config.h
@@ -0,0 +1,10 @@
+//Wifi
+char ssid[] = "SSID";
+char pass[] = "PASS";
+//Urls
+String URL_temp = "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
+String URL_hum = "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
+String URL_tempout = "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
+String URL_radio =  "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
+String URL_track = "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
+String URL_cur = "http://10.10.2.10:8080/rest/items/YOUR_ITEM/state";
